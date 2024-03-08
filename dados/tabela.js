3edba2a44dbc2d9db2b6e143c417e86af82ecab1
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-03-08 20:19:18 -0300

    Envio da pasta contendo os dados

---
+++ dados/tabela.js
@@ -0,0 +1,18 @@
+const tabela = [
+    {decimal: 1000, romano: 'M'},
+    {decimal: 900, romano: 'CM'},
+    {decimal: 500, romano: 'D'},
+    {decimal: 400, romano: 'CD'},
+    {decimal: 100, romano: 'C'},
+    {decimal: 90, romano: 'XC'},
+    {decimal: 50, romano: 'L'},
+    {decimal: 40, romano: 'XL'},
+    {decimal: 10, romano: 'X'},
+    {decimal: 9, romano: 'IX'},
+    {decimal: 5, romano: 'V'},
+    {decimal: 4, romano: 'IV'},
+    {decimal: 1, romano: 'I'}
+];
+
+
+exports.tabela = tabela;
